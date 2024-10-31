1fb6a5b38060bfa10136b2682e236ffccc3a4dc1
JoonhyeongKim <jhkim@swfact.com>
2024-10-31 11:06:32 +0900

    snackbar

--- hamsung/lib/main.dart
+++ hamsung/lib/main.dart
@@ -58,18 +58,27 @@ class _HomeState extends State<Home> {
         if (data.containsKey('chapterList')) {
           final List<dynamic> chapterList = data['chapterList'];
 
-          // chapterList 출력 - 번호 붙여서 세로로 출력
           for (int i = 0; i < chapterList.length; i++) {
             print('${i}. ${chapterList[i]}');
           }
+
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Chapter List 불러오기가 완료되었습니다.')),
+          );
         } else {
-          print('chapterList 필드가 없습니다.');
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('chapterList 필드가 없습니다.')),
+          );
         }
       } else {
-        print('pid에 해당하는 문서를 찾을 수 없습니다.');
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('pid에 해당하는 문서를 찾을 수 없습니다.')),
+        );
       }
     } catch (e) {
-      print('chapterList를 가져오는 중 오류 발생: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('chapterList를 가져오는 중 오류 발생: $e')),
+      );
     }
   }
 
@@ -88,18 +97,27 @@ class _HomeState extends State<Home> {
         if (data.containsKey('quotaList')) {
           final List<dynamic> quotaList = data['quotaList'];
 
-          // quotaList 출력 - 번호 붙여서 세로로 출력
           for (int i = 0; i < quotaList.length; i++) {
             print('${i}. ${quotaList[i]}');
           }
+
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Quota List 불러오기가 완료되었습니다.')),
+          );
         } else {
-          print('quotaList 필드가 없습니다.');
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('quotaList 필드가 없습니다.')),
+          );
         }
       } else {
-        print('pid에 해당하는 문서를 찾을 수 없습니다.');
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('pid에 해당하는 문서를 찾을 수 없습니다.')),
+        );
       }
     } catch (e) {
-      print('quotaList를 가져오는 중 오류 발생: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('quotaList를 가져오는 중 오류 발생: $e')),
+      );
     }
   }
 
@@ -120,7 +138,6 @@ class _HomeState extends State<Home> {
           final List<dynamic> quotaList =
               List.from(data['quotaList']); // 기존 리스트 가져오기
 
-          // 리스트의 범위를 초과하지 않는지 확인
           if (index >= 0 && index < quotaList.length) {
             for (int i = index; i < quotaList.length; i++) {
               if (quotaList[i] is int) {
@@ -128,20 +145,29 @@ class _HomeState extends State<Home> {
               }
             }
 
-            // Firestore에 업데이트
             await docRef.update({'quotaList': quotaList});
-            print('quotaList의 $index 이후 값이 +1 되었습니다.');
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('Quota List의 $index 이후 값이 +1 되었습니다.')),
+            );
           } else {
-            print('지정된 인덱스가 리스트 범위를 벗어났습니다.');
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('지정된 인덱스가 리스트 범위를 벗어났습니다.')),
+            );
           }
         } else {
-          print('quotaList 필드가 없습니다.');
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('quotaList 필드가 없습니다.')),
+          );
         }
       } else {
-        print('pid에 해당하는 문서를 찾을 수 없습니다.');
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('pid에 해당하는 문서를 찾을 수 없습니다.')),
+        );
       }
     } catch (e) {
-      print('quotaList를 업데이트하는 중 오류 발생: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('quotaList를 업데이트하는 중 오류 발생: $e')),
+      );
     }
   }
 
@@ -163,25 +189,31 @@ class _HomeState extends State<Home> {
           final List<dynamic> chapterList =
               List.from(data['chapterList']); // 기존 리스트 가져오기
 
-          // 리스트의 범위를 초과하지 않는지 확인
           if (index >= 0 && index <= chapterList.length) {
-            // 지정된 위치에 새로운 chapter 삽입
             chapterList.insert(index, newChapter);
-
-            // Firestore에 업데이트
             await docRef.update({'chapterList': chapterList});
-            print('새로운 chapter가 $index 위치에 추가되었습니다.');
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('새로운 chapter가 $index 위치에 추가되었습니다.')),
+            );
           } else {
-            print('지정된 인덱스가 리스트 범위를 벗어났습니다.');
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('지정된 인덱스가 리스트 범위를 벗어났습니다.')),
+            );
           }
         } else {
-          print('chapterList 필드가 없습니다.');
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('chapterList 필드가 없습니다.')),
+          );
         }
       } else {
-        print('pid에 해당하는 문서를 찾을 수 없습니다.');
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('pid에 해당하는 문서를 찾을 수 없습니다.')),
+        );
       }
     } catch (e) {
-      print('chapterList를 업데이트하는 중 오류 발생: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('chapterList를 업데이트하는 중 오류 발생: $e')),
+      );
     }
   }
 
@@ -205,7 +237,7 @@ class _HomeState extends State<Home> {
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: TextField(
-                controller: _indexController, // chapterList 인덱스 입력 필드
+                controller: _indexController,
                 keyboardType: TextInputType.number,
                 decoration: InputDecoration(
                   labelText: 'Chapter List 삽입할 인덱스 입력',
@@ -215,8 +247,7 @@ class _HomeState extends State<Home> {
             ),
             ElevatedButton(
               onPressed: () {
-                int index =
-                    int.parse(_indexController.text); // 사용자가 입력한 인덱스를 가져옴
+                int index = int.parse(_indexController.text);
                 addChapterAtPosition(
                     'kcFfOUg9R2VverIwOCmm', 'New Chapter Title', index);
               },
@@ -226,7 +257,7 @@ class _HomeState extends State<Home> {
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: TextField(
-                controller: _quotaIndexController, // quotaList 인덱스 입력 필드
+                controller: _quotaIndexController,
                 keyboardType: TextInputType.number,
                 decoration: InputDecoration(
                   labelText: 'Quota List에서 인덱스 입력',
@@ -236,8 +267,7 @@ class _HomeState extends State<Home> {
             ),
             ElevatedButton(
               onPressed: () {
-                int index =
-                    int.parse(_quotaIndexController.text); // 사용자가 입력한 인덱스를 가져옴
+                int index = int.parse(_quotaIndexController.text);
                 incrementQuotaListAfterIndex('kcFfOUg9R2VverIwOCmm', index);
               },
               child: Text('Quota List에서 해당 인덱스 이후 값 +1 하기'),
